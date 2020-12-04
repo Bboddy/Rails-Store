6ca7b00ad960f7d14ff7039eab67d4ead3b1f8f4
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-04 03:54:06 -0800

    Categories link_to setup
    Also fixed/added css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
-  resources :categories
+  resources :categories do
+    resources :products
+  end
 end
