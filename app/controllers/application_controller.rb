e51caee5e494c4d88459e32badffd8e2c3c255da
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-08 03:37:05 -0800

    Merg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :current_cart
     def current_cart
-        @current_cart ||= ShoppingCart.new(token: cart_token)
+        @current_cart ||= Cart.new
+        @current_cart.user = current_user
     end
     helper_method :current_cart
-    
-    private
-
-    def cart_token
-        return @cart_token unless @cart_token.nil?
-
-        session[:cart_token] ||= SecureRandom.hex(8)
-        @cart_token = session[:cart_token]
-    end
 end
