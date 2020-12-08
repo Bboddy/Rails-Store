713aae66d6dbdcf2613bcb419b660f5cb3334671
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-08 03:37:12 -0800

    Revert "Merg"
    This reverts commit e51caee5e494c4d88459e32badffd8e2c3c255da.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,16 +3,21 @@ class Cart < ApplicationRecord
     has_many :products
     belongs_to :order, optional: true
 
-    # def initialize()
+    def initialize(token:)
+        @token = token
+    end
 
-    # end
+    def order
+        @order ||= Order.find_or_create_by(token: @token) do |order|
+            order.total_price = 0
+    end
 
-    # def add_item(product_id:)
-    #     product = Product.find(product_id)
-    #     order_item = order.items.find_or_create_by(product_id: product_id)
+    def add_item(product_id:, quanity: 1)
+        product = Product.find(product_id)
+        order_item = order.items.find_or_create_by(product_id: product_id)
 
-    #     order_item.price = product.price
-    #     order_item.quanity = quanity
-    #     order_item.save
-    # end
+        order_item.price = product.price
+        order_item.quanity = quanity
+        order_item.save
+    end
 end
