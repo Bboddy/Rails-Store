dd0643d3f1fe72beda8276d5cc96c58d1aefe828
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-09 12:38:28 -0800

    Create WIP

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,15 +5,20 @@ class CartController < ApplicationController
 
     def create
         @cart = Cart.find_or_create_by(user: current_user)
-        product = Product.find(params[:product_id])
-        @item = Item.create!(cart: @cart, quanity: params[:quantiy], product: product)
+        @product = Product.find(params[:product_id])
+        @item = Item.find_or_create_by(product: @product, cart: @cart)
+        @item.update(quanity: params[:quanity])
 
-        redirect_to cart_path(@cart.id)
+        # redirect_to cart_path(@cart.id)
     end
 
     def show
         @cart = Cart.find_or_create_by(user: current_user)
         @items = Item.where(cart: @cart)
         render 'carts/show'
+    end
+
+    def update
+        
     end
 end
