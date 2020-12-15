b8595562eee6680dadff58616a86d9c5d90ab19e
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-15 02:43:44 -0800

    Cart Total Update

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,7 +7,8 @@ class CartController < ApplicationController
         cart = Cart.find_or_create_by(user: current_user)
         product = Product.find(params[:product_id])
         item = Item.find_or_create_by(product: product, cart: cart, quanity: params[:quanity])
-        cart.total_price = (i.product.price.to_i * i.quanity.to_i)
+        cart.total_price = (item.product.price.to_i * item.quanity.to_i)
+        update_total_price()
         redirect_to cart_path(cart.id)
     end
 
@@ -22,16 +23,15 @@ class CartController < ApplicationController
         product = Product.find(params[:product_id])
         item = Item.find_or_create_by(product: product, cart: cart)
         total = 0
-        items = Item.where(cart: cart)
-        items.each do |i|
-            total += (i.product.price.to_i * i.quanity.to_i)
-        end
+        
         if item_params[:quanity].to_i < 1
             item.destroy
+            update_total_price()
         else
             item.update(item_params)
-            cart.update(total_price: total.to_i)
+            update_total_price()
         end
+
         redirect_to cart_path(cart.id)
     end
 
@@ -39,5 +39,17 @@ class CartController < ApplicationController
 
     def item_params
         params.require(:item).permit(:quanity)
+    end
+
+    def update_total_price
+        cart = Cart.find_or_create_by(user: current_user)
+        items = Item.where(cart: cart)
+        total = 0
+        
+        items.each do |i|
+            total += (i.product.price.to_i * i.quanity.to_i)
+        end
+
+        cart.update(total_price: total.to_i)
     end
 end
