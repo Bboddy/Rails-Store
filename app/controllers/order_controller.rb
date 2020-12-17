a678c8980f8361b274ad7523ffc20cc78496d788
Bboddy <43559704+Bboddy@users.noreply.github.com>
2020-12-17 08:26:09 -0800

    New Order WIP
    New order page made still wip

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,7 @@
 class OrderController < ApplicationController
-    def new 
+    def new
+        cart = Cart.find_or_create_by(user: current_user)
+        @items = Item.where(cart: cart)
         render 'orders/new'
     end
 end
